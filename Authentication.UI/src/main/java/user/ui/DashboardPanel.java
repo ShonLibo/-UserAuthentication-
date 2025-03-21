3e5395d30893f9214ed9468f4aed9d1b2e8a2993
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-03-22 01:33:32 +0300

    Dashboard Panel

---
+++ Authentication.UI/src/main/java/user/ui/DashboardPanel.java
@@ -0,0 +1,49 @@
+package user.ui;
+
+
+import musiccatalogue.UserAuthApp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DashboardPanel extends JPanel {
+    private UserAuthApp app;
+
+    public DashboardPanel(UserAuthApp app) {
+        this.app = app;
+        setBackground(new Color(2, 2, 46));
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        // Title
+        JLabel titleLabel = new JLabel("Dashboard", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titleLabel.setForeground(Color.WHITE);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        add(titleLabel, gbc);
+
+        // Back Button
+        JButton backButton = new JButton("Back To Sign In");
+        backButton.setFont(new Font("Arial", Font.BOLD, 14));
+        backButton.setBackground(new Color(40, 167, 69));
+        backButton.setForeground(Color.WHITE);
+        backButton.setFocusPainted(false);
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app.showPanel("LOGIN");
+            }
+        });
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        add(backButton, gbc);
+    }
+}
